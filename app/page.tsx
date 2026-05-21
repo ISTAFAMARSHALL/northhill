228ba8fc18b2a8d239b4ba76f19d046de88d62c4
ISTAFAMARSHALL <istafa.a.marshall@gmail.com>
2026-05-21 00:29:00 -0400

    update nav bar to fix mobile display error with logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import NorthHillLanding from "./components/NorthHillLanding";
 import NorthHillLanding2 from "./components/NorthHillLanding_8";
+import NorthHillLanding3 from "./components/NorthHillLanding_11";
 
 export default function Home() {
-  return <NorthHillLanding2 />;
+  return <NorthHillLanding3 />;
 }
